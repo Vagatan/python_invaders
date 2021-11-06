2fa7bbd063598041d6ed69f4ec0dcf79ccde4bbd
Grzegorz Kozak <grzesiek.kozak@gmail.com>
2021-11-06 06:34:52 +0100

    P-3 Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,31 +3,39 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry."""
 
     def __init__(self):
-        """Inicjalizacja gry i utworzenia jej zasobów"""
+        """Inicjalizacja gry i utworzenia jej zasobów."""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja obcych")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Rozpoczęcie pętli głównej gry."""
         while True:
-            # Oczekiwanie na naciśnięcie klawisza lub przycisku myszki
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Odświeżenie ekranu w trakcie każdej iteracji pętli
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """Reakcja na zdarzenia generowane przez klawiaturę i mysz."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Wyświetlenie ostatnio zmodyfkowanego ekranu
-            pygame.display.flip()
+    def _update_screen(self):
+        """Uaktualnienie obrazów na ekranie i przejście do nowego ekranu."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
