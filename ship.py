aa6c2df4768d74166642c0dba8a40319ba54bc46
Grzegorz Kozak <grzesiek.kozak@gmail.com>
2021-11-07 07:52:46 +0100

    P-4 Moving left

--- ship.py
+++ ship.py
@@ -18,11 +18,14 @@ class Ship:
 
         # Opcje wskazujące na poruszanie się statku
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """Uaktualnienie położenia statku na podstawie opcji wskazującej na jego ruch"""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Wyświetlanie statku kosmicznego w jego aktualnym położeniu"""
