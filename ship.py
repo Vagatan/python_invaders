c4f74c31445a32a2a5dd3e398bcafcdfb870a05b
Grzegorz Kozak <grzesiek.kozak@gmail.com>
2021-11-07 08:12:10 +0100

    P-4 Added ship movement limitations

--- ship.py
+++ ship.py
@@ -27,9 +27,9 @@ class Ship:
     def update(self):
         """Uaktualnienie położenia statku na podstawie opcji wskazującej na jego ruch"""
         # Uaktualnienie wartości współrzednej X statki  nie jego prostokąta
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Uaktualnienie obiektu rect na podstawie wartości self.x
